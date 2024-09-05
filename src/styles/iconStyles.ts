e5cdb97943b9095b72216f1abbdff5d8264f5535
정호연 <jea7730@gmail.com>
2024-09-06 00:11:18 +0900

    :lipstick: 정호연 : 반응형 디자인으로 코드 수정

--- src/styles/iconStyles.ts
+++ src/styles/iconStyles.ts
@@ -1,10 +1,10 @@
 const iconStyles = {
   container: 'p-2 relative',
-  iconWrapper: 'fixed bottom-5 right-5 flex items-center space-x-4',
-  icon: 'cursor-pointer bg-white rounded-full transition-all duration-300 hover:bg-gray-200',
+  iconWrapper: 'fixed bottom-5 right-5 md:bottom-10 md:right-10 flex items-center space-x-4',
+  icon: 'cursor-pointer bg-white rounded-full transition-all duration-300 hover:bg-gray-200 w-12 h-12 md:w-16 md:h-16',
   hoverText: 'bg-gray-200 px-4 py-2 rounded text-sm',
   toastMessage:
-    'fixed bottom-5 left-1/2 transform -translate-x-1/2 text-sm px-4 py-2 bg-black/70 text-white rounded-lg shadow-lg transition-opacity duration-500',
+    'fixed bottom-5 left-1/2 transform -translate-x-1/2 text-sm px-4 py-2 bg-black/70 text-white rounded-lg shadow-lg transition-opacity duration-500 sm:bottom-8 md:bottom-10 whitespace-nowrap overflow-hidden text-ellipsis',
 };
 
 export default iconStyles;
