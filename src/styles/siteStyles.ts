ce77af60694ab0e7ebff515e48661309d96bb4a9
정호연 <jea7730@gmail.com>
2024-09-06 01:32:42 +0900

    :lipstick: 정호연 : flex-col 삭제

--- src/styles/siteStyles.ts
+++ src/styles/siteStyles.ts
@@ -3,7 +3,7 @@ const siteStyles = {
   hoverActive: 'bg-black text-white',
   hoverInactive: 'bg-white text-black',
   hoverText: 'mt-2 text-gray-500 text-sm md:text-base transition-all duration-300',
-  siteListContainer: 'flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4',
+  siteListContainer: 'flex space-x-4 md:flex-row md:space-y-0 md:space-x-4',
 };
 
 export default siteStyles;
