95c4093b20adb3a9cc7cb42e4e09d3d694d2291c
정호연 <jea7730@gmail.com>
2024-09-06 00:04:48 +0900

    :lipstick: 정호연 : icon styles

---
+++ src/styles/iconStyles.ts
@@ -0,0 +1,10 @@
+const iconStyles = {
+  container: 'p-2 relative',
+  iconWrapper: 'fixed bottom-5 right-5 flex items-center space-x-4',
+  icon: 'cursor-pointer bg-white rounded-full transition-all duration-300 hover:bg-gray-200',
+  hoverText: 'bg-gray-200 px-4 py-2 rounded text-sm',
+  toastMessage:
+    'fixed bottom-5 left-1/2 transform -translate-x-1/2 text-sm px-4 py-2 bg-black/70 text-white rounded-lg shadow-lg transition-opacity duration-500',
+};
+
+export default iconStyles;
