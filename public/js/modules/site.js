d849253f1dc529493aa64b4c14f820e1dea11461
정호연 <jea7730@gmail.com>
2024-09-05 16:37:15 +0900

    :pencil2: 정호연 : 코드 위치 변경

--- public/js/modules/site.js
+++ public/js/modules/site.js
@@ -1,16 +1,16 @@
 export const site = (site_name, site_link) => {
   const tab_menu = document.getElementById('tab-menu');
+
   const bundle = document.createElement('div');
   bundle.className = 'bundle';
   tab_menu.appendChild(bundle);
 
   let site = document.createElement('div');
-  let hover_site = document.createElement('div');
   bundle.appendChild(site);
-
   site.textContent = site_name;
   site.style.cursor = 'pointer';
 
+  let hover_site = document.createElement('div');
   hover_site.textContent = `${site_name} 보러가기`;
 
   site.addEventListener('click', () => {
