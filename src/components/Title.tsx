c73162ac6d0d5f1ecfaeb529df5c87701b8418d6
정호연 <jea7730@gmail.com>
2024-09-06 01:35:49 +0900

    :lipstick: 정호연 : 타이틀을 정중앙에 위치

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Title: React.FC = () => {
   return (
-    <h1 className="text-7xl font-normal text-gray-900">
-      개발자 <span className="font-bold">정호연</span>입니다.
-    </h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-normal text-gray-900">
+        개발자 <span className="font-bold">정호연</span>입니다.
+      </h1>
+    </div>
   );
 };
 
