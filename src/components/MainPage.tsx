d108c6b37b4cc9e48c5114939aacc302f6c0677e
정호연 <jea7730@gmail.com>
2024-09-06 01:15:29 +0900

    :green_heart: 정호연 : Title 컴포넌트 조립

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SiteList from './SiteList';
 import IconList from './IconList';
+import Title from './Title';
 
 const MainPage: React.FC = () => {
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <SiteList />
       <IconList />
+      <Title />
     </div>
   );
 };
