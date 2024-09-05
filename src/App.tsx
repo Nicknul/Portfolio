f0b462a05aa941264be788d5f1234504de494a07
정호연 <jea7730@gmail.com>
2024-09-05 21:26:49 +0900

    :white_check_mark: 정호연 : tailwind css 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-// src/App.tsx
 import React from 'react';
 
 const App: React.FC = () => {
-  return <h1>Hello, TypeScript with React!</h1>;
+  return <h1 className="bg-blue-500 px-4 py-2 rounded-lg">Hello, World</h1>;
 };
 
 export default App;
