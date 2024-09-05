0489df6b119e82b76fef10ccdd07183641eba648
정호연 <jea7730@gmail.com>
2024-09-05 23:25:06 +0900

    :sparkles: 정호연 : Toast 메시지 추가

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -11,12 +11,14 @@ interface IconProps {
 
 const Icon: React.FC<IconProps> = ({ iconName, iconTextNode, imgSrc, imgAlt, imgWidth, imgHeight }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
 
   const handleCopy = () => {
     navigator.clipboard
       .writeText(iconTextNode)
       .then(() => {
-        alert(`${iconTextNode}이 복사되었습니다.`);
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 3000);
       })
       .catch((err) => {
         console.error('Failed to copy text: ', err);
@@ -24,19 +26,26 @@ const Icon: React.FC<IconProps> = ({ iconName, iconTextNode, imgSrc, imgAlt, img
   };
 
   return (
-    <div className="p-2 flex flex-col items-end">
+    <div className="p-2 flex items-center justify-center relative space-x-4">
+      {isHovered && <div className="bg-gray-200 px-4 py-2 rounded text-sm">{iconTextNode}</div>}
       <img
         src={imgSrc}
         alt={imgAlt}
         width={imgWidth}
         height={imgHeight}
-        id="icons"
         onClick={handleCopy}
         onMouseOver={() => setIsHovered(true)}
         onMouseOut={() => setIsHovered(false)}
-        className="cursor-pointer bg-white rounded-full hover:bg-gray-200 transition-all duration-300"
+        className="cursor-pointer bg-white rounded-full transition-all duration-300 hover:bg-gray-200"
       />
-      {isHovered && <div className="bg-gray-200 p-2 rounded">{iconTextNode}</div>}
+
+      <div
+        className={`text-sm px-4 py-2 fixed bottom-5 left-1/2 transform -translate-x-1/2 bg-black/70 text-white p-2 rounded-lg shadow-lg transition-opacity duration-500 ${
+          isCopied ? 'opacity-100' : 'opacity-0'
+        }`}
+      >
+        {iconTextNode}이 복사되었습니다.
+      </div>
     </div>
   );
 };
