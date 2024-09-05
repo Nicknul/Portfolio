f9f1b5cb43eb2e8b47a80eba0dbddc10b0afccef
정호연 <jea7730@gmail.com>
2024-09-05 12:17:03 +0900

    :hammer: 정호연 : bundle 요소 추가

--- public/js/tab/github.js
+++ public/js/tab/github.js
@@ -1,9 +1,12 @@
 export const github_main = () => {
   const tab_menu = document.getElementById('tab-menu');
+  const bundle = document.createElement('div');
+  bundle.className = 'bundle';
+  tab_menu.appendChild(bundle);
 
   let github = document.createElement('div');
   let hover_github = document.createElement('div');
-  tab_menu.appendChild(github);
+  bundle.appendChild(github);
 
   github.textContent = 'GitHub';
   github.style.cursor = 'pointer';
@@ -11,18 +14,18 @@ export const github_main = () => {
   hover_github.textContent = 'GitHub 보러가기';
 
   github.addEventListener('click', () => {
-    window.location.href = 'https://github.com/Nicknul';
+    window.open('https://github.com/Nicknul', '_blank');
   });
 
   github.addEventListener('mouseover', () => {
     github.style.backgroundColor = 'black';
     github.style.color = 'white';
-    tab_menu.appendChild(hover_github);
+    bundle.appendChild(hover_github);
   });
 
   github.addEventListener('mouseout', () => {
     github.style.backgroundColor = 'white';
     github.style.color = 'black';
-    tab_menu.removeChild(hover_github);
+    bundle.removeChild(hover_github);
   });
 };
