b992b8894a0d5cd599f5625c44a131d6f4c9350a
정호연 <jea7730@gmail.com>
2024-09-06 00:39:33 +0900

    :lipstick: 정호연 : site style file

---
+++ src/styles/siteStyles.ts
@@ -0,0 +1,9 @@
+const siteStyles = {
+  siteContainer: 'px-4 py-2 rounded-lg transition-all duration-300 cursor-pointer w-[150px] md:w-[200px]',
+  hoverActive: 'bg-black text-white',
+  hoverInactive: 'bg-white text-black',
+  hoverText: 'mt-2 text-gray-500 text-sm md:text-base',
+  siteListContainer: 'flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4',
+};
+
+export default siteStyles;
