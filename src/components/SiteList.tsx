5d3e918882840ac87315bad294299c38202a67de
정호연 <jea7730@gmail.com>
2024-09-05 22:15:09 +0900

    :recycle: 정호연 : site 컴포넌트 import 후 사이트 기재

---
+++ src/components/SiteList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Site from './Site';
+
+const sites = [
+  { siteName: 'GitHub', siteLink: 'https://github.com/Nicknul' },
+  {
+    siteName: 'Notion',
+    siteLink:
+      'https://stupendous-lighter-a2b.notion.site/3285a71a0daf4475b8dabd1ca260604d?v=fc4dafec8dc8454098541b1f489dd710',
+  },
+];
+
+const SiteList: React.FC = () => {
+  return (
+    <div className="flex space-x-4">
+      {sites.map((site, index) => (
+        <Site key={index} siteName={site.siteName} siteLink={site.siteLink} />
+      ))}
+    </div>
+  );
+};
+
+export default SiteList;
