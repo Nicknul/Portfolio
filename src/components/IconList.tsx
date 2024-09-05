531858b5588014f2d4c490700ad1cfb927556461
정호연 <jea7730@gmail.com>
2024-09-05 22:31:50 +0900

    :recycle: 정호연 : side.js 컴포넌트 리펙토링

---
+++ src/components/IconList.tsx
@@ -0,0 +1,34 @@
+// src/components/IconList.tsx
+import React from 'react';
+import Icon from './Icon';
+
+const iconsData = [
+  {
+    iconName: 'E-mail',
+    iconTextNode: 'example@example.com',
+    imgSrc: '../../imgs/email.png',
+    imgAlt: 'E-mail',
+    imgWidth: 48,
+    imgHeight: 48,
+  },
+];
+
+const IconList: React.FC = () => {
+  return (
+    <div id="side-menu" className="flex flex-col items-end">
+      {iconsData.map((icon, index) => (
+        <Icon
+          key={index}
+          iconName={icon.iconName}
+          iconTextNode={icon.iconTextNode}
+          imgSrc={icon.imgSrc}
+          imgAlt={icon.imgAlt}
+          imgWidth={icon.imgWidth}
+          imgHeight={icon.imgHeight}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default IconList;
