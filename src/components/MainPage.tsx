e9938e472ffc3c7c2f83ad0cf6d007ac6dcb6dc3
정호연 <jea7730@gmail.com>
2024-09-05 22:28:20 +0900

    :green_heart: 정호연 : IconList 모듈 조립

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SiteList from './SiteList';
+import IconList from './IconList';
 
 const MainPage: React.FC = () => {
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <SiteList />
+      <IconList />
     </div>
   );
 };
