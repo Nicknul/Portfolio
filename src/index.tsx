89fbaa758aa561b531a4801913bf33870e846847
정호연 <jea7730@gmail.com>
2024-09-05 23:08:28 +0900

    :hammer: 정호연 : react-dom/client 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 // src/index.tsx
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './globals.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement!); // createRoot로 루트 설정
+root.render(<App />);
