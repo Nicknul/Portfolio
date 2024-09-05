61563958e2c91fa12714516175cd3ea63baebd75
정호연 <jea7730@gmail.com>
2024-09-05 15:13:23 +0900

    :green_heart: 정호연 : side 컴포넌트 조립

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
 import { site } from './modules/site.js';
+import { side } from './modules/side.js';
 
 site('GitHub', 'https://github.com/Nicknul');
 site(
   'Notion',
   'https://stupendous-lighter-a2b.notion.site/3285a71a0daf4475b8dabd1ca260604d?v=fc4dafec8dc8454098541b1f489dd710&pvs=4'
 );
+
+side('E-Mail', 'jea7730@gamil.com');
