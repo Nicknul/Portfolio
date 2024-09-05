f7ef6671c8176ac1cc40739a884cadc5011a2d73
정호연 <jea7730@gmail.com>
2024-09-05 12:21:53 +0900

    :hammer: 정호연 : 매개변수를 사용하여 모듈화

---
+++ public/js/modules/site.js
@@ -0,0 +1,31 @@
+export const site = (site_name, site_link) => {
+  const tab_menu = document.getElementById('tab-menu');
+  const bundle = document.createElement('div');
+  bundle.className = 'bundle';
+  tab_menu.appendChild(bundle);
+
+  let site = document.createElement('div');
+  let hover_site = document.createElement('div');
+  bundle.appendChild(site);
+
+  site.textContent = site_name;
+  site.style.cursor = 'pointer';
+
+  hover_site.textContent = `${site_name} 보러가기`;
+
+  site.addEventListener('click', () => {
+    window.open(site_link, '_blank');
+  });
+
+  site.addEventListener('mouseover', () => {
+    site.style.backgroundColor = 'black';
+    site.style.color = 'white';
+    bundle.appendChild(hover_site);
+  });
+
+  site.addEventListener('mouseout', () => {
+    site.style.backgroundColor = 'white';
+    site.style.color = 'black';
+    bundle.removeChild(hover_site);
+  });
+};
