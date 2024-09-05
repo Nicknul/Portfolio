1605e2289ad62dde757b78dc7253c2ce19b4be04
정호연 <jea7730@gmail.com>
2024-09-05 23:43:37 +0900

    :lipstick: 정호연 : 아이콘 위치 고정

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -26,21 +26,22 @@ const Icon: React.FC<IconProps> = ({ iconName, iconTextNode, imgSrc, imgAlt, img
   };
 
   return (
-    <div className="p-2 flex items-center justify-center relative space-x-4">
-      {isHovered && <div className="bg-gray-200 px-4 py-2 rounded text-sm">{iconTextNode}</div>}
-      <img
-        src={imgSrc}
-        alt={imgAlt}
-        width={imgWidth}
-        height={imgHeight}
-        onClick={handleCopy}
-        onMouseOver={() => setIsHovered(true)}
-        onMouseOut={() => setIsHovered(false)}
-        className="cursor-pointer bg-white rounded-full transition-all duration-300 hover:bg-gray-200"
-      />
-
+    <div className="p-2 relative">
+      <div className="fixed bottom-5 right-5 flex items-center space-x-4">
+        {isHovered && <div className="bg-gray-200 px-4 py-2 rounded text-sm">{iconTextNode}</div>}
+        <img
+          src={imgSrc}
+          alt={imgAlt}
+          width={imgWidth}
+          height={imgHeight}
+          onClick={handleCopy}
+          onMouseOver={() => setIsHovered(true)}
+          onMouseOut={() => setIsHovered(false)}
+          className="cursor-pointer bg-white rounded-full transition-all duration-300 hover:bg-gray-200"
+        />
+      </div>
       <div
-        className={`text-sm px-4 py-2 fixed bottom-5 left-1/2 transform -translate-x-1/2 bg-black/70 text-white p-2 rounded-lg shadow-lg transition-opacity duration-500 ${
+        className={`fixed bottom-5 left-1/2 transform -translate-x-1/2 text-sm px-4 py-2 bg-black/70 text-white rounded-lg shadow-lg transition-opacity duration-500 ${
           isCopied ? 'opacity-100' : 'opacity-0'
         }`}
       >
