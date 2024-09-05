79872b6de33424d04de0ad023f03c81b8ff95b9d
정호연 <jea7730@gmail.com>
2024-09-05 14:38:37 +0900

    :recycle: 정호연 : mouseover, mouseout envet module

---
+++ public/js/utils/MouseEvent.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Element} element_1 이벤트 적용 시킬 요소
+ * @param {Element} element_2 이벤트 발동 시 나타날 요소
+ */
+export const hover = (element_1, element_2, children = '') => {
+  element_1.addEventListener('mouseover', () => {
+    element_1.appendChild(element_2);
+    children;
+  });
+
+  element_1.addEventListener('mouseout', () => {
+    element_1.removeChild(element_2);
+    children;
+  });
+};
