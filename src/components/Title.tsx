35e6c610dfa106c2bef64903a7f41f1579abeaf1
정호연 <jea7730@gmail.com>
2024-09-06 01:35:13 +0900

    :lipstick: 정호연 : 정호연만 볼드 처리

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Title: React.FC = () => {
-  return <h1 className="text-3xl font-bold text-gray-900">개발자 정호연입니다.</h1>;
+  return (
+    <h1 className="text-7xl font-normal text-gray-900">
+      개발자 <span className="font-bold">정호연</span>입니다.
+    </h1>
+  );
 };
 
 export default Title;
