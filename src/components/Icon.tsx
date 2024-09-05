2a53e6a555026eee5111cae34eeb68e8a80d7d6c
정호연 <jea7730@gmail.com>
2024-09-05 23:53:45 +0900

    :green_heart: 정호연 : 컴포넌트, 모듈 조립

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,4 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
+import HoverText from './HoverText';
+import ToastMessage from './ToastMessage';
+import useClipboard from '../hooks/useClipboard';
+import useHover from '../hooks/useHover';
 
 interface IconProps {
   iconName: string;
@@ -10,43 +14,25 @@ interface IconProps {
 }
 
 const Icon: React.FC<IconProps> = ({ iconName, iconTextNode, imgSrc, imgAlt, imgWidth, imgHeight }) => {
-  const [isHovered, setIsHovered] = useState(false);
-  const [isCopied, setIsCopied] = useState(false);
-
-  const handleCopy = () => {
-    navigator.clipboard
-      .writeText(iconTextNode)
-      .then(() => {
-        setIsCopied(true);
-        setTimeout(() => setIsCopied(false), 3000);
-      })
-      .catch((err) => {
-        console.error('Failed to copy text: ', err);
-      });
-  };
+  const { isHovered, handleMouseOver, handleMouseOut } = useHover();
+  const { isCopied, copyToClipboard } = useClipboard(iconTextNode);
 
   return (
     <div className="p-2 relative">
       <div className="fixed bottom-5 right-5 flex items-center space-x-4">
-        {isHovered && <div className="bg-gray-200 px-4 py-2 rounded text-sm">{iconTextNode}</div>}
+        {isHovered && <HoverText text={iconTextNode} />}
         <img
           src={imgSrc}
           alt={imgAlt}
           width={imgWidth}
           height={imgHeight}
-          onClick={handleCopy}
-          onMouseOver={() => setIsHovered(true)}
-          onMouseOut={() => setIsHovered(false)}
+          onClick={copyToClipboard}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
           className="cursor-pointer bg-white rounded-full transition-all duration-300 hover:bg-gray-200"
         />
       </div>
-      <div
-        className={`fixed bottom-5 left-1/2 transform -translate-x-1/2 text-sm px-4 py-2 bg-black/70 text-white rounded-lg shadow-lg transition-opacity duration-500 ${
-          isCopied ? 'opacity-100' : 'opacity-0'
-        }`}
-      >
-        {iconTextNode}이 복사되었습니다.
-      </div>
+      <ToastMessage message={`${iconTextNode}이 복사되었습니다.`} isVisible={isCopied} />
     </div>
   );
 };
