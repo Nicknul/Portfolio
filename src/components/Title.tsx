b6376a4389b45464cf44144d1865b2546b8f7e5a
정호연 <jea7730@gmail.com>
2024-09-06 10:17:00 +0900

    :lipstick: 정호연 : 타이틀 위치 고정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="absolute inset-0 flex items-center justify-center z-10 pointer-events-none">
       <h1 className="text-3xl font-normal text-gray-900">
         개발자 <span className="font-bold">정호연</span>입니다.
       </h1>
