90d3f3d869d04e3786dacd9251e515de9ddd5b45
정호연 <jea7730@gmail.com>
2024-09-06 11:24:19 +0900

    :sparkles: 정호연 : 변경되는 문자열 애니메이션

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,10 +4,15 @@ const texts = ['개발자', '도전자', '유쾌한'];
 
 const Title: React.FC = () => {
   const [currentText, setCurrentText] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentText((prevText) => (prevText + 1) % texts.length);
+      setIsAnimating(true);
+      setTimeout(() => {
+        setCurrentText((prevText) => (prevText + 1) % texts.length);
+        setIsAnimating(false);
+      }, 500);
     }, 2000);
 
     return () => clearInterval(intervalId);
@@ -15,10 +20,18 @@ const Title: React.FC = () => {
 
   return (
     <div className="absolute inset-0 flex items-center justify-center z-10 pointer-events-none">
-      <h1 className="text-3xl font-normal text-gray-900 relative">
-        <span className="absolute overflow-hidden text-3xl whitespace-nowrap">{texts[currentText]}</span>
-        <span className="font-bold inline-block ml-24">정호연</span>
-        <span className="inline-block ml-4">입니다.</span>
+      <h1 className="text-3xl font-normal text-gray-900 relative flex items-center">
+        <div className="overflow-hidden relative mr-2 ">
+          <span
+            className={`block transition-transform duration-500 ${
+              isAnimating ? '-translate-y-full opacity-0' : 'translate-y-0 opacity-100'
+            }`}
+          >
+            {texts[currentText]}
+          </span>
+        </div>
+        <span className="font-bold ml-2">정호연</span>
+        <span className="ml-2">입니다.</span>
       </h1>
     </div>
   );
