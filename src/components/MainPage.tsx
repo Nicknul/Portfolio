c9c7d88f13523e40ea0f6c974de5d991a5711086
정호연 <jea7730@gmail.com>
2024-09-06 23:40:46 +0900

    :fire: 정호연 : 타이틀 컴포넌트 import 코드 삭제

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import SiteList from './SiteList';
 import IconList from './IconList';
-import Title from './Title';
 
 const MainPage: React.FC = () => {
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <SiteList />
-      <Title />
       <IconList />
     </div>
   );
