a92d9e52fdb57521fdfdc392faf9ae81e5554461
정호연 <jea7730@gmail.com>
2024-09-07 22:12:09 +0900

    :green_heart: 정호연 : 카드 리스트 조립

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SiteList from './SiteList';
 import IconList from './IconList';
+import CardList from './CardList';
 
 const MainPage: React.FC = () => {
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <SiteList />
       <IconList />
+      <CardList />
     </div>
   );
 };
