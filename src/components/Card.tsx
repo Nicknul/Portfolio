b6f777144569cc3781b07634215987f157238d43
정호연 <jea7730@gmail.com>
2024-09-09 11:56:35 +0900

    :fire: 정호연 : 주석 삭제

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,7 @@ type CardProps = {
   title: string;
   description: string;
   image: string;
-  index: number; // 카드의 순서를 위한 추가 속성
+  index: number;
 };
 
 const Card: React.FC<CardProps> = ({ title, description, image, index }) => {
@@ -14,7 +14,7 @@ const Card: React.FC<CardProps> = ({ title, description, image, index }) => {
       className={`bg-white rounded-lg shadow-lg p-4 w-64 absolute transform`}
       style={{
         zIndex: 10 - index,
-        left: `${index * 40}px`, // 오른쪽으로 겹쳐지도록 조정
+        left: `${index * 40}px`,
       }}
     >
       <img src={image} alt={title} className="w-full h-40 object-cover rounded-t-lg" />
