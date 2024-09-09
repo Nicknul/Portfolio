c6d53960228a84174876aaf66cd68359c6271bda
정호연 <jea7730@gmail.com>
2024-09-09 22:56:49 +0900

    :fire: 정호연 : 주석 삭제

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,8 +11,8 @@ const Card: React.FC<CardProps> = ({ title, category, image }) => {
     <div className="bg-white shadow-md rounded-lg overflow-hidden w-full sm:w-80 h-auto">
       <img src={image} alt={title} className="w-full h-40 object-cover" />
       <div className="p-4">
-        <span className="text-sm text-gray-500">{category}</span> {/* 분류 태그 */}
-        <h3 className="text-lg sm:text-xl font-semibold mt-2">{title}</h3> {/* 타이틀 */}
+        <span className="text-sm text-gray-500">{category}</span>
+        <h3 className="text-lg sm:text-xl font-semibold mt-2">{title}</h3>
       </div>
     </div>
   );
