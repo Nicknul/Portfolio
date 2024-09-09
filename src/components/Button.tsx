4495184e69a9cadcc158c23682adb31c28764e83
정호연 <jea7730@gmail.com>
2024-09-10 00:41:39 +0900

    :lipstick: 정호연 : 버튼 스타일 변경

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
+      className="border border-white text-white px-4 py-2 rounded-lg hover:bg-white hover:text-zinc-800 transition duration-300"
     >
       {label}
     </button>
