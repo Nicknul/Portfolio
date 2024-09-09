0e5ac01d4e44a8811ecb11bbf4c6162880a95ba9
정호연 <jea7730@gmail.com>
2024-09-09 17:34:46 +0900

    :lipstick: 정호연 : z-index 수정

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,3 @@
-// Card.tsx
 import React from 'react';
 
 type CardProps = {
@@ -6,11 +5,12 @@ type CardProps = {
   description: string;
   image: string;
   className?: string;
+  style?: React.CSSProperties;
 };
 
-const Card: React.FC<CardProps> = ({ title, description, image, className }) => {
+const Card: React.FC<CardProps> = ({ title, description, image, className, style }) => {
   return (
-    <div className={`bg-white shadow-lg rounded-lg p-4 w-80 h-80 ${className}`}>
+    <div className={`bg-white shadow-lg rounded-lg p-4 w-80 h-80 ${className}`} style={style}>
       <img src={image} alt={title} className="w-full h-40 object-cover rounded-t-lg" />
       <h3 className="text-lg font-semibold mt-2">{title}</h3>
       <p className="text-gray-600">{description}</p>
