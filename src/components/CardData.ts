6765d97030ab3c66966fc97ef78fc7d209613a1a
정호연 <jea7730@gmail.com>
2024-09-10 01:08:14 +0900

    :adhesive_bandage: 정호연 : 카테고리 내용 수정

--- src/components/CardData.ts
+++ src/components/CardData.ts
@@ -1,19 +1,19 @@
 export const cards = [
   {
     title: 'ERP 프로젝트',
-    category: 'Business',
+    category: '팀',
     image: 'https://via.placeholder.com/150',
     githubLink: 'https://github.com/KDT-IaaS-Class-Two-Group/KDT-IaaS-2-ProjectA-1team',
   },
   {
     title: '포켓몬 타입',
-    category: 'Entertainment',
+    category: '싱글',
     image: 'https://via.placeholder.com/150',
     githubLink: 'https://github.com/Nicknul/New-folder/tree/master/pokemon-types-link',
   },
   {
     title: '미세먼지 앱',
-    category: 'Weather',
+    category: '싱글',
     image: 'https://via.placeholder.com/150',
     githubLink: 'https://github.com/Nicknul/weather',
   },
