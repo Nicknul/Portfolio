64cc56da0a653539b8fc3a037e4865d67731c2f7
정호연 <jea7730@gmail.com>
2024-09-10 00:48:14 +0900

    :sparkles: 정호연 : className 타입 정의

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 type ButtonProps = {
   label: string;
   onClick: () => void;
+  className?: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ label, onClick, className }) => {
   return (
     <button
       onClick={onClick}
-      className="border border-white text-white px-4 py-2 rounded-lg hover:bg-white hover:text-zinc-800 transition duration-300"
+      className={`border border-white text-white px-4 py-2 rounded-lg hover:bg-white hover:text-zinc-800 transition duration-300 ${className}`}
     >
       {label}
     </button>
