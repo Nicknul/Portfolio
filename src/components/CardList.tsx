e6c5f56dd2d927cbb081249735148c6fc4108247
정호연 <jea7730@gmail.com>
2024-09-09 16:11:43 +0900

    :pencil2: 정호연 : 버튼 아이콘 변경

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -8,7 +8,7 @@ const CardList: React.FC = () => {
   return (
     <div className="flex items-center w-full">
       <Button variant="left" onClick={() => alert('왼쪽 버튼 클릭됨!')} className="hidden lg:block">
-        왼쪽 버튼
+        ◀️
       </Button>
       <div className="flex flex-wrap justify-center gap-4 flex-grow mx-4">
         {cards.map((card, index) => (
@@ -16,7 +16,7 @@ const CardList: React.FC = () => {
         ))}
       </div>
       <Button variant="right" onClick={() => alert('오른쪽 버튼 클릭됨!')} className="hidden lg:block">
-        오른쪽 버튼
+        ▶️
       </Button>
     </div>
   );
