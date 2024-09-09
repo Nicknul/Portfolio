100fd13bb753ac655aa0bd087f87978a3ba12094
정호연 <jea7730@gmail.com>
2024-09-10 01:06:37 +0900

    :sparkles: 정호연 : GitHub 보기 button > a 요소로 변경

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 
 type ButtonProps = {
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
+  href?: string;
   className?: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, className }) => {
-  return (
+const Button: React.FC<ButtonProps> = ({ label, onClick, href, className }) => {
+  return href ? (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`border border-white text-white px-4 py-2 rounded-lg hover:bg-white hover:text-zinc-800 transition duration-300 text-center ${className}`}
+    >
+      {label}
+    </a>
+  ) : (
     <button
       onClick={onClick}
-      className={`border border-white text-white px-4 py-2 rounded-lg hover:bg-white hover:text-zinc-800 transition duration-300 ${className}`}
+      className={`border border-white text-white px-4 py-2 rounded-lg hover:bg-white hover:text-zinc-800 transition duration-300 text-center ${className}`}
     >
       {label}
     </button>
