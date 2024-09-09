9e6601eea8ef8f1f905ba1128624288dd20cba7c
정호연 <jea7730@gmail.com>
2024-09-09 17:06:13 +0900

    :green_heart: 정호연 : 앱 화면일 경우 카드 포커스 삭제 Hook 조립

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -6,11 +6,11 @@ import Button from './Button';
 import useCardFocus from '../hooks/useCardFocus';
 
 const CardList: React.FC = () => {
-  const { focusedCard, focusLeft, focusRight } = useCardFocus(0, cards.length);
+  const { focusedCard, focusLeft, focusRight, isMobile } = useCardFocus(0, cards.length); // 훅 사용
 
   return (
     <div className="flex items-center w-full">
-      <Button variant="left" onClick={focusLeft} className="hidden lg:block">
+      <Button variant="left" onClick={focusLeft} className={`hidden lg:block ${isMobile ? 'pointer-events-none' : ''}`}>
         왼쪽 버튼
       </Button>
       <div className="flex flex-wrap justify-center gap-4 flex-grow mx-4">
@@ -21,12 +21,16 @@ const CardList: React.FC = () => {
             description={card.description}
             image={card.image}
             className={`transition-transform duration-300 ${
-              focusedCard === index ? 'transform translate-y-[-10px]' : ''
+              focusedCard === index && !isMobile ? 'transform translate-y-[-10px]' : ''
             }`}
           />
         ))}
       </div>
-      <Button variant="right" onClick={focusRight} className="hidden lg:block">
+      <Button
+        variant="right"
+        onClick={focusRight}
+        className={`hidden lg:block ${isMobile ? 'pointer-events-none' : ''}`}
+      >
         오른쪽 버튼
       </Button>
     </div>
