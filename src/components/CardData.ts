314ebe9b51dba78e6659613b3b9bd1e8b5f324f2
정호연 <jea7730@gmail.com>
2024-09-10 01:03:26 +0900

    :hammer: 정호연 : github link 항목 추가

--- src/components/CardData.ts
+++ src/components/CardData.ts
@@ -1,17 +1,20 @@
 export const cards = [
   {
     title: 'ERP 프로젝트',
-    category: '팀',
+    category: 'Business',
     image: 'https://via.placeholder.com/150',
+    githubLink: 'https://github.com/KDT-IaaS-Class-Two-Group/KDT-IaaS-2-ProjectA-1team',
   },
   {
     title: '포켓몬 타입',
-    category: '싱글',
+    category: 'Entertainment',
     image: 'https://via.placeholder.com/150',
+    githubLink: 'https://github.com/Nicknul/New-folder/tree/master/pokemon-types-link',
   },
   {
     title: '미세먼지 앱',
-    category: '싱글',
+    category: 'Weather',
     image: 'https://via.placeholder.com/150',
+    githubLink: 'https://github.com/Nicknul/weather',
   },
 ];
