64743388235b2f6fa7bdc2ceb0f4b0f38daee303
정호연 <jea7730@gmail.com>
2024-09-09 21:46:46 +0900

    :adhesive_bandage: 정호연 : Card Data 수정

--- src/components/CardData.ts
+++ src/components/CardData.ts
@@ -1,27 +1,17 @@
 export const cards = [
   {
-    title: 'ERP',
-    description: '이 프로젝트는 React를 사용한 예제입니다.',
+    title: 'ERP 프로젝트',
+    category: '팀',
     image: 'https://via.placeholder.com/150',
   },
   {
     title: '포켓몬 타입',
-    description: 'Poke API를 사용하여 타입별 포켓몬 홈페이지 제작',
+    category: '싱글',
     image: 'https://via.placeholder.com/150',
   },
   {
-    title: '콜록콜록, 우리동네 미세먼지',
-    description: '공공데이터포털 API를 활용하여 만든 날씨 앱',
-    image: 'https://via.placeholder.com/150',
-  },
-  {
-    title: '프로젝트 4',
-    description: '이 프로젝트는 API 연동 예제입니다.',
-    image: 'https://via.placeholder.com/150',
-  },
-  {
-    title: '프로젝트 5',
-    description: '이 프로젝트는 데이터 시각화 대시보드입니다.',
+    title: '미세먼지 앱',
+    category: '싱글',
     image: 'https://via.placeholder.com/150',
   },
 ];
