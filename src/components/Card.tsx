1fa3dc35fcf7d5ecebd4f1d7242b30f6b870902d
정호연 <jea7730@gmail.com>
2024-09-10 00:07:03 +0900

    :lipstick: 정호연 : 마우스 호버 시 단색으로 색상 변경

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,11 +8,17 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({ title, category, image }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg overflow-hidden w-full sm:w-80 h-auto transform transition-transform duration-300 hover:-translate-y-2 cursor-pointer">
-      <img src={image} alt={title} className="w-full h-40 object-cover" />
+    <div className="bg-white shadow-md rounded-lg overflow-hidden w-full sm:w-80 h-auto transform transition-transform duration-300 hover:-translate-y-2 cursor-pointer hover:bg-zinc-800 group">
+      <div className="relative w-full h-40 overflow-hidden">
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0"
+        />
+      </div>
       <div className="p-4">
-        <span className="text-sm text-gray-500">{category}</span>
-        <h3 className="text-lg sm:text-xl font-semibold mt-2">{title}</h3>
+        <span className="text-sm text-gray-500 group-hover:text-white">{category}</span>
+        <h3 className="text-lg sm:text-xl font-semibold mt-2 group-hover:text-white">{title}</h3>
       </div>
     </div>
   );
