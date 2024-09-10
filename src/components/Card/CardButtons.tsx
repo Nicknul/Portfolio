cff138ae104eaf2e9db20b1b9db9d5390ef2ea54
정호연 <jea7730@gmail.com>
2024-09-10 17:19:21 +0900

    :hammer: 정호연 : 매개변수 openModal 추가

--- src/components/Card/CardButtons.tsx
+++ src/components/Card/CardButtons.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Button from '../Button';
-import CardStyles from '../../styles/CardStyles';
 
 type CardButtonsProps = {
   githubLink: string;
   isSelected: boolean;
   isMobile: boolean;
+  openModal: () => void;
 };
 
-const CardButtons: React.FC<CardButtonsProps> = ({ githubLink, isSelected, isMobile }) => (
-  <div className={CardStyles.buttons(isSelected, isMobile)}>
-    <Button label="자세히 보기" className={CardStyles.buttonBase} />
-    <Button label="GitHub 보기" href={githubLink} className={CardStyles.buttonBase} />
+const CardButtons: React.FC<CardButtonsProps> = ({ githubLink, isSelected, isMobile, openModal }) => (
+  <div
+    className={`absolute inset-0 flex flex-col justify-center items-center space-y-2 transition-all duration-300 ${
+      isMobile ? (isSelected ? 'block' : 'hidden') : 'opacity-0 group-hover:opacity-100'
+    }`}
+  >
+    <Button label="자세히 보기" className="w-full max-w-[150px]" onClick={openModal} />
+    <Button label="GitHub 보기" href={githubLink} className="w-full max-w-[150px]" />
   </div>
 );
 
