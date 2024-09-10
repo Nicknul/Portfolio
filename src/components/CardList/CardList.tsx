068da401f77460dec066b2e033ba0c615f861e4b
정호연 <jea7730@gmail.com>
2024-09-10 17:16:48 +0900

    :green_heart: 정호연 : Modal 컴포넌트 조립

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Card from '../Card/Card';
+import Modal from '../Modal/Modal';
 import { cards } from '../../data/CardData';
 import useIsMobile from './useIsMobile';
 
 const CardList: React.FC = () => {
   const [selectedCardIndex, setSelectedCardIndex] = useState<number | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const isMobile = useIsMobile();
 
   const handleCardClick = (index: number) => {
@@ -13,21 +15,39 @@ const CardList: React.FC = () => {
     }
   };
 
+  const openModal = (index: number) => {
+    setSelectedCardIndex(index);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      {cards.map((card, index) => (
-        <Card
-          key={index}
-          title={card.title}
-          category={card.category}
-          image={card.image}
-          githubLink={card.githubLink}
-          isSelected={selectedCardIndex === index && isMobile}
-          onClick={() => handleCardClick(index)}
-          isMobile={isMobile}
-        />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            title={card.title}
+            category={card.category}
+            image={card.image}
+            githubLink={card.githubLink}
+            isSelected={selectedCardIndex === index && isMobile}
+            onClick={() => handleCardClick(index)}
+            isMobile={isMobile}
+            openModal={() => openModal(index)}
+          />
+        ))}
+      </div>
+      {selectedCardIndex !== null && (
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <h2 className="text-xl font-semibold">{cards[selectedCardIndex].title}</h2>
+          <p className="mt-4">{cards[selectedCardIndex].category}</p>
+        </Modal>
+      )}
+    </>
   );
 };
 
