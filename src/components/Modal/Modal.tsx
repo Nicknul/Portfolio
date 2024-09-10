d9ddd805f70c7934c4d4bb171929d439d54d3a70
정호연 <jea7730@gmail.com>
2024-09-10 21:09:51 +0900

    :sparkles: 정호연 : 이미지 영역 추가

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,22 +4,25 @@ type ModalProps = {
   isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
+  imageUrl: string;
 };
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, imageUrl }) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div className="bg-white w-4/5 h-4/5 rounded-lg shadow-lg relative">
+      <div className="bg-white w-4/5 h-4/5 rounded-lg shadow-lg relative overflow-hidden">
         <button
           onClick={onClose}
           className="absolute top-4 right-4 bg-gray-300 hover:bg-gray-400 text-gray-700 px-4 py-2 rounded-lg"
         >
           닫기
         </button>
-
-        <div className="p-6">{children}</div>
+        <div className="w-full h-2/4 bg-gray-200">
+          <img src={imageUrl} alt="Modal 이미지" className="object-cover w-full h-full" />
+        </div>
+        <div className="p-6 h-2/3 overflow-y-auto">{children}</div>
       </div>
     </div>
   );
