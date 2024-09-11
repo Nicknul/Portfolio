a76ed16451569e05d34c702b941a527d93e4ad40
정호연 <jea7730@gmail.com>
2024-09-11 23:19:55 +0900

    :green_heart: 정호연 : 모달 슬라이더 컴포넌트 조립

--- src/components/Modal/ModalImageGallery.tsx
+++ src/components/Modal/ModalImageGallery.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import ModalImageSlider from './ModalImageSlider';
 
 type ModalImageGalleryProps = {
   images: string[];
+  isMobile: boolean;
 };
 
-const ModalImageGallery: React.FC<ModalImageGalleryProps> = ({ images }) => {
-  return (
+const ModalImageGallery: React.FC<ModalImageGalleryProps> = ({ images, isMobile }) => {
+  return isMobile ? (
+    <ModalImageSlider images={images} />
+  ) : (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       {images.map((image, index) => (
         <img key={index} src={image} alt={`설계 이미지 ${index + 1}`} className="w-full h-auto object-cover" />
