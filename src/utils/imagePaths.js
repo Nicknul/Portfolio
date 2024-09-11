3affbd4c584a6423a2cd7151fb22a6c2ea86b327
정호연 <jea7730@gmail.com>
2024-09-11 18:44:39 +0900

    :sparkles: 정호연 : 폴더명과 이미지명 추출

--- src/utils/imagePaths.js
+++ src/utils/imagePaths.js
@@ -5,22 +5,22 @@ const projectRoot = path.join(__dirname, '../..');
 const imageDir = path.join(projectRoot, 'public/imgs');
 console.log(imageDir);
 
-const getImagesByProjectName = (projectName) => {
-  const projectDir = path.join(imageDir, projectName);
+const getImagesFromFolder = (folderPath, projectName) =>
+  fs
+    .readdirSync(folderPath)
+    .filter((file) => ['.png', '.jpg', '.jpeg'].includes(path.extname(file).toLowerCase()))
+    .map((file) => path.join(`/imgs/${projectName}`, file));
 
-  if (fs.existsSync(projectDir)) {
-    const images = fs
-      .readdirSync(projectDir)
-      .filter((file) => ['.png', '.jpg', '.jpeg'].includes(path.extname(file).toLowerCase()))
-      .map((file) => path.join(`/imgs/${projectName}`, file));
+export const getAllProjectImages = () => {
+  const directories = fs.readdirSync(imageDir).filter((name) => fs.lstatSync(path.join(imageDir, name)).isDirectory());
 
-    return images;
-  } else {
-    console.log(`폴더가 존재하지 않습니다: ${projectName}`);
-    return [];
-  }
+  const allImages = directories.reduce((acc, projectName) => {
+    const projectDir = path.join(imageDir, projectName);
+    acc[projectName] = getImagesFromFolder(projectDir, projectName);
+    return acc;
+  }, {});
+
+  return allImages;
 };
 
-const cardTitle = 'ERP';
-const imagesForProject = getImagesByProjectName(cardTitle);
-console.log(imagesForProject);
+const allProjectImages = getAllProjectImages();
