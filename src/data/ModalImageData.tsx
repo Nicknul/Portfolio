9bf1218cdb1a2d349c367a49c8ba5c2aa68feb1d
정호연 <jea7730@gmail.com>
2024-09-11 18:52:37 +0900

    :adhesive_bandage: 정호연 : ERP 이미지 경로 수정

--- src/data/ModalImageData.tsx
+++ src/data/ModalImageData.tsx
@@ -1,8 +1,17 @@
-// ModalImageData.ts
 export const modalImages = {
   erp: [
-    'https://via.placeholder.com/600x400?text=ERP+Design+1',
-    'https://via.placeholder.com/600x400?text=ERP+Design+2',
+    '/imgs/ERP/drawio.png',
+    '/imgs/ERP/google-sheet.png',
+    '/imgs/ERP/figma.png',
+    '/imgs/ERP/ERP_main.png',
+    '/imgs/ERP/insert.png',
+    '/imgs/ERP/create-table.png',
+    '/imgs/ERP/template.png',
+    '/imgs/ERP/add-column.png',
+    '/imgs/ERP/add-row.png',
+    '/imgs/ERP/deleted.png',
+    '/imgs/ERP/setting.png',
+    '/imgs/ERP/password.png',
   ],
   pokemon: [
     'https://via.placeholder.com/600x400?text=Pokemon+Design+1',
