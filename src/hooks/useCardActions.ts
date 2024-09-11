c3525a59b0d618ddb67f79f63ef2c4f898985789
정호연 <jea7730@gmail.com>
2024-09-11 16:19:38 +0900

    :recycle: 정호연 : switch 및 상태 관리 hook

---
+++ src/hooks/useCardActions.ts
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { toggleData } from '../data/ToggleData';
+import { modalImages } from '../data/ModalImageData';
+
+const useCardActions = () => {
+  const [selectedCardIndex, setSelectedCardIndex] = useState<number | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = (index: number) => {
+    setSelectedCardIndex(index);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCardClick = (index: number, isMobile: boolean) => {
+    if (isMobile) {
+      setSelectedCardIndex(index);
+    }
+  };
+
+  const getToggleData = (index: number) => {
+    switch (index) {
+      case 0:
+        return toggleData.erp;
+      case 1:
+        return toggleData.pokemon;
+      case 2:
+        return toggleData.dustApp;
+      default:
+        return [];
+    }
+  };
+
+  const getModalImages = (index: number) => {
+    switch (index) {
+      case 0:
+        return modalImages.erp;
+      case 1:
+        return modalImages.pokemon;
+      case 2:
+        return modalImages.dustApp;
+      default:
+        return [];
+    }
+  };
+
+  return {
+    selectedCardIndex,
+    isModalOpen,
+    openModal,
+    closeModal,
+    handleCardClick,
+    getToggleData,
+    getModalImages,
+  };
+};
+
+export default useCardActions;
