2f0ecc4f12020030a5b53f62cad34e9f987abc35
정호연 <jea7730@gmail.com>
2024-09-11 19:00:32 +0900

    :lipstick: 정호연 : 모바일에서 이미지 배치 수정

--- src/components/Modal/ModalImageGallery.tsx
+++ src/components/Modal/ModalImageGallery.tsx
@@ -6,9 +6,9 @@ type ModalImageGalleryProps = {
 
 const ModalImageGallery: React.FC<ModalImageGalleryProps> = ({ images }) => {
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       {images.map((image, index) => (
-        <img key={index} src={image} alt={`설계 이미지 ${index + 1}`} className="w-full h-auto object-cover rounded" />
+        <img key={index} src={image} alt={`설계 이미지 ${index + 1}`} className="w-full h-auto object-cover" />
       ))}
     </div>
   );
