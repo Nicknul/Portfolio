f3362c9c0754fd1117211978f776d544f176385c
정호연 <jea7730@gmail.com>
2024-09-12 21:13:03 +0900

    :lipstick: 정호연 : 마우스 커셔 포인터

--- src/components/Modal/ModalImageGallery.tsx
+++ src/components/Modal/ModalImageGallery.tsx
@@ -12,7 +12,12 @@ const ModalImageGallery: React.FC<ModalImageGalleryProps> = ({ images, isMobile 
   ) : (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       {images.map((image, index) => (
-        <img key={index} src={image} alt={`설계 이미지 ${index + 1}`} className="w-full h-auto object-cover" />
+        <img
+          key={index}
+          src={image}
+          alt={`설계 이미지 ${index + 1}`}
+          className="w-full h-auto object-cover hover:cursor-pointer"
+        />
       ))}
     </div>
   );
