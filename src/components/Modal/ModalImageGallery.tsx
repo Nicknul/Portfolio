ec7d19b6c1e7a563bc9a5ec185e806c26eff8d0b
정호연 <jea7730@gmail.com>
2024-09-12 22:31:41 +0900

    :sparkles: 정호연 : 자세히 보기 버튼 추가

--- src/components/Modal/ModalImageGallery.tsx
+++ src/components/Modal/ModalImageGallery.tsx
@@ -14,7 +14,12 @@ const ModalImageGallery: React.FC<ModalImageGalleryProps> = ({ images, isMobile 
       {images.map((image, index) => (
         <div key={index} className="relative group w-full h-auto">
           <img src={image} alt={`설계 이미지 ${index + 1}`} className="w-full h-auto object-cover rounded-lg" />
-          <div className="absolute inset-0 bg-black/30 opacity-0 group-hover:opacity-100 backdrop-blur-lg transition-opacity duration-300 hover:cursor-pointer rounded-lg"></div>
+          <div className="absolute inset-0 bg-black/30 opacity-0 group-hover:opacity-100 backdrop-blur-lg transition-opacity duration-300 rounded-lg"></div>
+          <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <button className="border border-white text-white px-4 py-2 rounded-lg transition-colors duration-300 hover:bg-white hover:text-black">
+              자세히 보기
+            </button>
+          </div>
         </div>
       ))}
     </div>
