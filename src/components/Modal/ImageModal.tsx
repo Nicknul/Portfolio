75c46b6a6e053c0b19b431ecb41c6604bd6d7c7e
정호연 <jea7730@gmail.com>
2024-09-13 00:12:32 +0900

    :lipstick: 정호연 : 닫기 버튼 위치 조정

--- src/components/Modal/ImageModal.tsx
+++ src/components/Modal/ImageModal.tsx
@@ -10,10 +10,13 @@ const ImageModal: React.FC<ImageModalProps> = ({ imageUrl, onClose }) => {
     <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50">
       <div className="relative">
         <img src={imageUrl} alt="확대된 이미지" className="max-w-[90vw] max-h-[90vh] object-contain" />
-        <button onClick={onClose} className="absolute top-4 right-4 text-white text-xl">
-          닫기
-        </button>
       </div>
+      <button
+        onClick={onClose}
+        className="fixed top-4 right-4 text-white text-xl bg-black bg-opacity-50 rounded-full px-3 py-1"
+      >
+        닫기
+      </button>
     </div>
   );
 };
