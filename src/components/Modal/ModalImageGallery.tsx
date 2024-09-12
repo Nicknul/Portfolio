8c94b9621d6fdf2d2b0d5e179ee4d7b1da419354
정호연 <jea7730@gmail.com>
2024-09-12 21:44:05 +0900

    :lipstick: 정호연 : 이미지 모서리 라운드 처리

--- src/components/Modal/ModalImageGallery.tsx
+++ src/components/Modal/ModalImageGallery.tsx
@@ -13,8 +13,8 @@ const ModalImageGallery: React.FC<ModalImageGalleryProps> = ({ images, isMobile 
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       {images.map((image, index) => (
         <div key={index} className="relative group w-full h-auto">
-          <img src={image} alt={`설계 이미지 ${index + 1}`} className="w-full h-auto object-cover" />
-          <div className="absolute inset-0 bg-black/30 opacity-0 group-hover:opacity-100 backdrop-blur-sm transition-opacity duration-300 hover:cursor-pointer"></div>
+          <img src={image} alt={`설계 이미지 ${index + 1}`} className="w-full h-auto object-cover rounded-lg" />
+          <div className="absolute inset-0 bg-black/30 opacity-0 group-hover:opacity-100 backdrop-blur-lg transition-opacity duration-300 hover:cursor-pointer rounded-lg"></div>
         </div>
       ))}
     </div>
