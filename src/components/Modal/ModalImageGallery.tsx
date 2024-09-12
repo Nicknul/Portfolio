b1ef750f11cd46da9b80da697c17b45754300122
정호연 <jea7730@gmail.com>
2024-09-12 21:24:58 +0900

    :lipstick: 정호연 : 이미지에 마우스 호버 시 불투명 단색 색상 덮기

--- src/components/Modal/ModalImageGallery.tsx
+++ src/components/Modal/ModalImageGallery.tsx
@@ -12,12 +12,14 @@ const ModalImageGallery: React.FC<ModalImageGalleryProps> = ({ images, isMobile 
   ) : (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       {images.map((image, index) => (
-        <img
-          key={index}
-          src={image}
-          alt={`설계 이미지 ${index + 1}`}
-          className="w-full h-auto object-cover hover:cursor-pointer"
-        />
+        <div key={index} className="relative group w-full h-auto">
+          <img
+            src={image}
+            alt={`설계 이미지 ${index + 1}`}
+            className="w-full h-auto object-cover hover:cursor-pointer"
+          />
+          <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
+        </div>
       ))}
     </div>
   );
