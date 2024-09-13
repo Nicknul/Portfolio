9727bbab48335f45dcba8bae5489927b8c852a4b
정호연 <jea7730@gmail.com>
2024-09-13 12:54:59 +0900

    :sparkles: 정호연 : 프로젝트 메인 페이지

---
+++ src/components/ProjectPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardList from './Card/CardList';
+
+const ProjectPage: React.FC = () => {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <CardList />
+    </div>
+  );
+};
+
+export default ProjectPage;
