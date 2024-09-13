20ab67affeab02524e116234df9214de6a19c12b
정호연 <jea7730@gmail.com>
2024-09-13 22:49:54 +0900

    :green_heart: 정호연 : 상단바 컴포넌트 조립

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavItem from './NavItem';
+
+const NavBar: React.FC = () => {
+  return (
+    <nav className="bg-white shadow-md p-4">
+      <ul className="flex space-x-6 md:space-x-24 lg:space-x-40 justify-center text-gray-900">
+        <NavItem label="Home" href="#home" />
+        <NavItem label="Resume" href="#resume" />
+        <NavItem label="Portfolio" href="#portfolio" />
+        <NavItem label="GitHub" href="https://github.com/Nicknul" newTab={true} />
+        <NavItem
+          label="Notion"
+          href="https://stupendous-lighter-a2b.notion.site/3285a71a0daf4475b8dabd1ca260604d?v=fc4dafec8dc8454098541b1f489dd710&pvs=4"
+          newTab={true}
+        />
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
