b0c784d114d87b80bba6dc164532142f5cb02560
정호연 <jea7730@gmail.com>
2024-09-14 01:48:39 +0900

    :lipstick: 정호연 : 여백 수정

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -5,9 +5,11 @@ import Toggle from '../components/Toggle/Toggle';
 const PortfolioPage: React.FC = () => {
   return (
     <div className="pt-16 min-h-[calc(100vh-4rem)]">
-      <Toggle />
-      {/* <CardList /> */}
-      <div className="p-20">Portfolio Page</div>
+      <div className="w-full lg:max-w-[40%] mx-auto">
+        <Toggle />
+        {/* <CardList /> */}
+        <div className="p-20">Portfolio Page</div>
+      </div>
     </div>
   );
 };
