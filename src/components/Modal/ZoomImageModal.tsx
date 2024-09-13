b55c63d645f9caabe49df87cf6f24ff535086d98
정호연 <jea7730@gmail.com>
2024-09-13 12:00:52 +0900

    :sparkles: 정호연 : 확대된 이미지 모달창에서 이미지 넘기기

--- src/components/Modal/ZoomImageModal.tsx
+++ src/components/Modal/ZoomImageModal.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { useSwipeable } from 'react-swipeable';
 
 type ZoomImageModalProps = {
-  image: string;
+  images: string[];
+  currentIndex: number;
   onClose: () => void;
+  onNext: () => void;
+  onPrev: () => void;
 };
 
-const ZoomImageModal: React.FC<ZoomImageModalProps> = ({ image, onClose }) => {
+const ZoomImageModal: React.FC<ZoomImageModalProps> = ({ images, currentIndex, onClose, onNext, onPrev }) => {
+  const handlers = useSwipeable({
+    onSwipedLeft: onNext,
+    onSwipedRight: onPrev,
+    preventScrollOnSwipe: true,
+    trackMouse: true,
+  });
+
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50">
-      <img src={image} alt="확대된 이미지" className="max-w-[90vw] max-h-[90vh] object-contain" />
+    <div {...handlers} className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50">
+      <img
+        src={images[currentIndex]}
+        alt={`확대된 이미지 ${currentIndex + 1}`}
+        className="max-w-[85vw] max-h-[85vh] object-contain"
+      />
       <button
         onClick={onClose}
         className="absolute top-4 right-4 text-white bg-black bg-opacity-50 px-3 py-1 rounded-full z-50"
