c53e48f2fff6e6ca8446c867913c9e4e7a55e40e
정호연 <jea7730@gmail.com>
2024-09-13 12:06:45 +0900

    :green_heart: 정호연 : 훅, 컴포넌트 조립

--- src/components/Modal/ModalImageSlider.tsx
+++ src/components/Modal/ModalImageSlider.tsx
@@ -1,51 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSwipeable } from 'react-swipeable';
 import ZoomImageModal from './ZoomImageModal';
+import useImageSlider from '../../hooks/useImageSlider';
+import ImageCard from './ImageCard';
 
 type ModalImageSliderProps = {
   images: string[];
 };
 
 const ModalImageSlider: React.FC<ModalImageSliderProps> = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isZoomed, setIsZoomed] = useState(false);
-  const [zoomedIndex, setZoomedIndex] = useState<number | null>(null);
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
-  };
-
-  const handleZoomNext = () => {
-    if (zoomedIndex !== null) {
-      setZoomedIndex((prevIndex) => {
-        if (prevIndex === null) return 0;
-        return (prevIndex + 1) % images.length;
-      });
-    }
-  };
-
-  const handleZoomPrev = () => {
-    if (zoomedIndex !== null) {
-      setZoomedIndex((prevIndex) => {
-        if (prevIndex === null) return images.length - 1;
-        return prevIndex === 0 ? images.length - 1 : prevIndex - 1;
-      });
-    }
-  };
-
-  const handleImageClick = () => {
-    setIsZoomed(true);
-    setZoomedIndex(currentIndex);
-  };
-
-  const handleZoomClose = () => {
-    setIsZoomed(false);
-    setZoomedIndex(null);
-  };
+  const {
+    currentIndex,
+    isZoomed,
+    zoomedIndex,
+    handleNext,
+    handlePrev,
+    handleZoomNext,
+    handleZoomPrev,
+    handleImageClick,
+    handleZoomClose,
+  } = useImageSlider(images);
 
   const handlers = useSwipeable({
     onSwipedLeft: handleNext,
@@ -56,18 +30,15 @@ const ModalImageSlider: React.FC<ModalImageSliderProps> = ({ images }) => {
 
   return (
     <>
-      {/* 카드 모달 컨텐츠 */}
       <div {...handlers} className="relative w-full h-auto">
         <div className="relative w-full h-64 overflow-hidden">
           {images.map((image, index) => (
-            <img
+            <ImageCard
               key={index}
-              src={image}
-              alt={`설계 이미지 ${index + 1}`}
-              className={`absolute top-0 left-0 w-full h-full object-contain transition-transform duration-500 ${
-                index === currentIndex ? 'translate-x-0' : 'translate-x-full'
-              }`}
+              image={image}
+              altText={`설계 이미지 ${index + 1}`}
               onClick={handleImageClick}
+              isVisible={index === currentIndex}
             />
           ))}
         </div>
