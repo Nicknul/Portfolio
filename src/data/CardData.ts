b33aa55f3a359be92c31144309fafc3e607a10f7
정호연 <jea7730@gmail.com>
2024-09-13 16:08:03 +0900

    :hammer: 정호연 : description 추가

--- src/data/CardData.ts
+++ src/data/CardData.ts
@@ -1,10 +1,11 @@
 export const cards = [
   {
-    title: 'ERP 프로젝트',
+    title: 'ERP',
     category: '팀',
     image: '/imgs/ERP/ERP_main.png',
     githubLink: 'https://github.com/KDT-IaaS-Class-Two-Group/KDT-IaaS-2-ProjectA-1team',
-    description: '회사 분야에 제약 없이 사용할 수 있는 ERP 프로그램을 제작',
+    description:
+      '이 프로젝트는 회사의 분야에 제약 없이 사용할 수 있는 범용 ERP 프로그램을 제작하는 것을 목표로 ERP는 다양한 부서 간 데이터를 통합하여 효율적인 관리와 운영을 가능하게 하는 프로그램을 만드는 것이었습니다.',
   },
   {
     title: '포켓몬 타입',
@@ -14,7 +15,7 @@ export const cards = [
     description: '',
   },
   {
-    title: '미세먼지 앱',
+    title: '우리동네 날씨',
     category: '싱글',
     image: 'https://via.placeholder.com/150',
     githubLink: 'https://github.com/Nicknul/weather',
