522b44a6b465c6b312a9c2bd2d6c38d219cd9e89
정호연 <jea7730@gmail.com>
2024-09-14 02:30:24 +0900

    :sparkles: 정호연 : 선택한 카테고리를 관리

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import Toggle from '../components/Toggle/Toggle';
+import React, { useState } from 'react';
+import Toggle from '../components/Toggle/CategoryToggle';
 import CardList from '../components/Card/CardList';
 
 const PortfolioPage: React.FC = () => {
+  const [selectedCategory, setSelectedCategory] = useState<'팀프로젝트' | '싱글' | '전체'>('전체');
+
   return (
     <div className="pt-16 min-h-[calc(100vh-4rem)]">
       <div className="w-full lg:max-w-[40%] mx-auto">
-        <Toggle />
-        <CardList />
+        <Toggle selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        <CardList category={selectedCategory} />
       </div>
     </div>
   );
