ac96c88ad602e8a491a86c1811a894113f58c9b8
정호연 <jea7730@gmail.com>
2024-09-13 12:55:16 +0900

    :hammer: 정호연 : 프로젝트 페이지 조립

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import SiteList from './SiteList';
 import IconList from './IconList';
-import CardList from './Card/CardList';
+import ProjectPage from './ProjectPage';
 
 const MainPage: React.FC = () => {
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
-      <SiteList />
-      <CardList />
-      <IconList />
-    </div>
+    <>
+      <div className="p-6 bg-gray-100 min-h-screen">
+        <SiteList />
+        <IconList />
+      </div>
+      <div className="p-6 bg-gray-100 min-h-screen">
+        <ProjectPage />
+      </div>
+    </>
   );
 };
 
