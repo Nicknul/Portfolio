d822dc8757d58cf267d36cf64e6e21988e928dfd
정호연 <jea7730@gmail.com>
2024-09-14 01:58:18 +0900

    :bulb: 정호연 : 코드 비활성화

--- src/components/Card/CardListContent.tsx
+++ src/components/Card/CardListContent.tsx
@@ -1,52 +1,52 @@
-import React from 'react';
-import Card from '../Card/Card';
-import Modal from '../Modal/Modal';
-import ModalContent from '../Modal/ModalContent';
-import { CardListContentProps } from '../../types/CardProps';
-import CardStyles from '../../styles/CardStyles';
+// import React from 'react';
+// import Card from '../Card/Card';
+// import Modal from '../Modal/Modal';
+// import ModalContent from '../Modal/ModalContent';
+// import { CardListContentProps } from '../../types/CardProps';
+// import CardStyles from '../../styles/CardStyles';
 
-const CardListContent: React.FC<CardListContentProps> = ({
-  cards,
-  selectedCardIndex,
-  isMobile,
-  handleCardClick,
-  openModal,
-  closeModal,
-  isModalOpen,
-  getToggleData,
-  getModalImages,
-}) => {
-  return (
-    <>
-      <div className={CardStyles.gridContainer}>
-        {cards.map((card, index) => (
-          <Card
-            key={index}
-            title={card.title}
-            category={card.category}
-            image={card.image}
-            githubLink={card.githubLink}
-            isSelected={selectedCardIndex === index && isMobile}
-            onClick={() => handleCardClick(index, isMobile)}
-            isMobile={isMobile}
-            openModal={() => openModal(index)}
-          />
-        ))}
-      </div>
+// const CardListContent: React.FC<CardListContentProps> = ({
+//   cards,
+//   selectedCardIndex,
+//   isMobile,
+//   handleCardClick,
+//   openModal,
+//   closeModal,
+//   isModalOpen,
+//   getToggleData,
+//   getModalImages,
+// }) => {
+//   return (
+//     <>
+//       <div className={CardStyles.gridContainer}>
+//         {cards.map((card, index) => (
+//           <Card
+//             key={index}
+//             title={card.title}
+//             category={card.category}
+//             image={card.image}
+//             githubLink={card.githubLink}
+//             isSelected={selectedCardIndex === index && isMobile}
+//             onClick={() => handleCardClick(index, isMobile)}
+//             isMobile={isMobile}
+//             openModal={() => openModal(index)}
+//           />
+//         ))}
+//       </div>
 
-      {selectedCardIndex !== null && (
-        <Modal isOpen={isModalOpen} onClose={closeModal} imageUrl={cards[selectedCardIndex].image}>
-          <ModalContent
-            title={cards[selectedCardIndex].title}
-            description={cards[selectedCardIndex].description}
-            toggleData={getToggleData(selectedCardIndex)}
-            images={getModalImages(selectedCardIndex)}
-            isMobile={isMobile}
-          />
-        </Modal>
-      )}
-    </>
-  );
-};
+//       {selectedCardIndex !== null && (
+//         <Modal isOpen={isModalOpen} onClose={closeModal} imageUrl={cards[selectedCardIndex].image}>
+//           <ModalContent
+//             title={cards[selectedCardIndex].title}
+//             description={cards[selectedCardIndex].description}
+//             toggleData={getToggleData(selectedCardIndex)}
+//             images={getModalImages(selectedCardIndex)}
+//             isMobile={isMobile}
+//           />
+//         </Modal>
+//       )}
+//     </>
+//   );
+// };
 
-export default CardListContent;
+// export default CardListContent;
