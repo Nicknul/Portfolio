948451270b5a289ba12fe16ef76eef82cf25481e
정호연 <jea7730@gmail.com>
2024-09-13 16:17:13 +0900

    :fire: 정호연 : 카테고리 컴포넌트 관련 코드 삭제

--- src/components/Card/CardList.tsx
+++ src/components/Card/CardList.tsx
@@ -2,22 +2,16 @@ import React from 'react';
 import useIsMobile from '../../hooks/useIsMobile';
 import useCardActions from '../../hooks/useCardActions';
 import useCategory from '../../hooks/useCategory';
-import CategoryTabs from './CategoryTabs';
 import CardListContent from './CardListContent';
 
 const CardList: React.FC = () => {
   const isMobile = useIsMobile();
   const { selectedCardIndex, isModalOpen, openModal, closeModal, handleCardClick, getToggleData, getModalImages } =
     useCardActions();
-  const { categories, selectedCategory, filteredCards, handleCategoryChange } = useCategory();
+  const { filteredCards } = useCategory();
 
   return (
     <>
-      <CategoryTabs
-        categories={categories}
-        selectedCategory={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
       <CardListContent
         cards={filteredCards}
         selectedCardIndex={selectedCardIndex}
