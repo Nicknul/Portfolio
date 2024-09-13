0f20ee36167a3feac5d7a8d06c083ffaa5fd942c
정호연 <jea7730@gmail.com>
2024-09-14 00:12:46 +0900

    :sparkles: 정호연 : React Router Link를 사용하여 페이지 전환

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavItem from './NavItem';
 
 const NavBar: React.FC = () => {
   return (
     <nav className="bg-white shadow-md p-4 fixed top-0 left-0 w-full z-50">
       <ul className="flex space-x-6 md:space-x-24 lg:space-x-40 justify-center text-gray-900">
-        <NavItem label="Home" href="#home" />
-        <NavItem label="Resume" href="#resume" />
-        <NavItem label="Portfolio" href="#portfolio" />
+        <NavItem label="Home" to="/" />
+        <NavItem label="Resume" to="/resume" />
+        <NavItem label="Portfolio" to="/portfolio" />
         <NavItem label="GitHub" href="https://github.com/Nicknul" newTab={true} />
         <NavItem
           label="Notion"
