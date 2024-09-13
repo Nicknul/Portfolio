74132c018ea47a2e1e0105b33625d1c79bb623b6
정호연 <jea7730@gmail.com>
2024-09-14 02:27:23 +0900

    :recycle: 정호연 : 카테고리별 필터링된 카드 보여주기

--- src/components/Card/CardList.tsx
+++ src/components/Card/CardList.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import CardItem from './CardItem';
 import { cardData } from '../../data/CardData';
 
-const CardList: React.FC = () => {
+interface CardListProps {
+  category: '팀프로젝트' | '싱글' | '전체';
+}
+
+const CardList: React.FC<CardListProps> = ({ category }) => {
+  const filteredCards = category === '전체' ? cardData : cardData.filter((card) => card.category === category);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
-      {cardData.map((card, index) => (
+      {filteredCards.map((card, index) => (
         <CardItem
           key={index}
           image={card.image}
