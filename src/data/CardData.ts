abd1156a1fa938631aa984786811ab6271735bee
정호연 <jea7730@gmail.com>
2024-09-13 15:42:30 +0900

    :hammer: 정호연 : 설명 추가

--- src/data/CardData.ts
+++ src/data/CardData.ts
@@ -4,17 +4,20 @@ export const cards = [
     category: '팀',
     image: '/imgs/ERP/ERP_main.png',
     githubLink: 'https://github.com/KDT-IaaS-Class-Two-Group/KDT-IaaS-2-ProjectA-1team',
+    description: '회사 분야에 제약 없이 사용할 수 있는 ERP 프로그램을 제작',
   },
   {
     title: '포켓몬 타입',
     category: '싱글',
     image: 'https://via.placeholder.com/150',
     githubLink: 'https://github.com/Nicknul/New-folder/tree/master/pokemon-types-link',
+    description: '',
   },
   {
     title: '미세먼지 앱',
     category: '싱글',
     image: 'https://via.placeholder.com/150',
     githubLink: 'https://github.com/Nicknul/weather',
+    description: '',
   },
 ];
