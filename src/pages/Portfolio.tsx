9bbf3d02ee89e12d0bb62dd3bc86ef58672a58c3
정호연 <jea7730@gmail.com>
2024-09-14 01:28:43 +0900

    :green_heart: 정호연 : Toggle 컴포넌트 조립, 페이지 높이 조정

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import CardList from '../components/Card/CardList';
+import Toggle from '../components/Toggle/Toggle';
 
 const PortfolioPage: React.FC = () => {
-  return <div className="p-20">Portfolio Page</div>;
+  return (
+    <div className="pt-16 min-h-[calc(100vh-4rem)]">
+      <Toggle />
+      {/* <CardList /> */}
+      <div className="p-20">Portfolio Page</div>
+    </div>
+  );
 };
 
 export default PortfolioPage;
