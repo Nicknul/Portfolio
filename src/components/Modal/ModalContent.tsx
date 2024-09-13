04eb45373aa705338f4b4ff5747b36d3848b3b05
정호연 <jea7730@gmail.com>
2024-09-13 15:39:39 +0900

    :sparkles: 정호연 : 갤러리 타이틀 추가

--- src/components/Modal/ModalContent.tsx
+++ src/components/Modal/ModalContent.tsx
@@ -13,19 +13,15 @@ type ModalContentProps = {
 const ModalContent: React.FC<ModalContentProps> = ({ title, description, toggleData, images, isMobile }) => {
   return (
     <div className="p-6 space-y-6">
-      {' '}
-      {/* 컨테이너에 간격 적용 */}
-      {/* 타이틀 섹션 */}
       <div className="border-b pb-4">
         <h2 className="text-2xl font-semibold mb-2">{title}</h2>
         <p className="text-gray-700">{description}</p>
       </div>
-      {/* 토글 섹션 */}
       <div className="border-b pb-4">
         <ToggleList data={toggleData} />
       </div>
-      {/* 이미지 갤러리 섹션 */}
       <div className="pt-4">
+        <h3 className="text-gray-700 font-semibold mb-2">갤러리</h3>
         <ModalImageGallery images={images} isMobile={isMobile} />
       </div>
     </div>
