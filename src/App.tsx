3625a4902e7568cae956cd7079f1764b8d418875
정호연 <jea7730@gmail.com>
2024-09-13 18:01:11 +0900

    :green_heart: 정호연 : 상단바 컴포넌트 조립

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MainPage from './components/MainPage';
+import NavBar from './components/NavBar/NavBar';
 
 const App: React.FC = () => {
   return (
     <div>
+      <NavBar />
       <MainPage />
       {/* 나중에 추가될 다른 컴포넌트들을 여기에 조립 */}
     </div>
