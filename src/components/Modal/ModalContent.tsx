a2887ca6926534f736c9450fb5ba4cf27311090e
정호연 <jea7730@gmail.com>
2024-09-13 15:35:37 +0900

    :hammer: 정호연 : 모달 컨텐츠 구역 나누기

--- src/components/Modal/ModalContent.tsx
+++ src/components/Modal/ModalContent.tsx
@@ -12,11 +12,22 @@ type ModalContentProps = {
 
 const ModalContent: React.FC<ModalContentProps> = ({ title, description, toggleData, images, isMobile }) => {
   return (
-    <div>
-      <h2 className="text-xl font-semibold">{title}</h2>
-      <p className="mt-4">{description}</p>
-      <ToggleList data={toggleData} />
-      <ModalImageGallery images={images} isMobile={isMobile} />
+    <div className="p-6 space-y-6">
+      {' '}
+      {/* 컨테이너에 간격 적용 */}
+      {/* 타이틀 섹션 */}
+      <div className="border-b pb-4">
+        <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+        <p className="text-gray-700">{description}</p>
+      </div>
+      {/* 토글 섹션 */}
+      <div className="border-b pb-4">
+        <ToggleList data={toggleData} />
+      </div>
+      {/* 이미지 갤러리 섹션 */}
+      <div className="pt-4">
+        <ModalImageGallery images={images} isMobile={isMobile} />
+      </div>
     </div>
   );
 };
