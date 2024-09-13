cb6b5ffa1cbbfd851e8c9dae3b08aa34740c4f35
정호연 <jea7730@gmail.com>
2024-09-13 10:24:40 +0900

    :sparkles: 정호연 : 썸네일 추가

--- src/components/Modal/ImageModal.tsx
+++ src/components/Modal/ImageModal.tsx
@@ -6,17 +6,31 @@ type ImageModalProps = {
   onClose: () => void;
   onPrev: () => void;
   onNext: () => void;
+  onSelectImage: (index: number) => void;
 };
 
-const ImageModal: React.FC<ImageModalProps> = ({ images, selectedIndex, onClose, onPrev, onNext }) => {
+const ImageModal: React.FC<ImageModalProps> = ({ images, selectedIndex, onClose, onPrev, onNext, onSelectImage }) => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50">
-      <div className="relative">
+    <div className="fixed inset-0 bg-black bg-opacity-80 flex flex-col justify-center items-center z-50">
+      <div className="relative flex flex-col items-center">
         <img
           src={images[selectedIndex]}
           alt={`확대된 이미지 ${selectedIndex + 1}`}
-          className="max-w-[90vw] max-h-[90vh] object-contain"
+          className="max-w-[85vw] max-h-[85vh] object-contain mb-4"
         />
+        <div className="flex space-x-2 overflow-visible">
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`썸네일 이미지 ${index + 1}`}
+              onClick={() => onSelectImage(index)}
+              className={`w-16 h-16 object-cover cursor-pointer rounded-lg transition-transform duration-300 ${
+                index === selectedIndex ? '-translate-y-2' : ''
+              }`}
+            />
+          ))}
+        </div>
       </div>
       <button
         onClick={onPrev}
