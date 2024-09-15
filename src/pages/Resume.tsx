e94c797f215a0bd27f224105a012480573f34d37
정호연 <jea7730@gmail.com>
2024-09-16 00:14:41 +0900

    :green_heart: 정호연 : ResumeContent 컴포넌트 조립

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import ResumeContent from '../components/Resume/ResumeContent';
 
 const ResumePage: React.FC = () => {
-  return <div className="p-20">Resume Page</div>;
+  return (
+    <div className="pt-16 min-h-[calc(100vh-4rem)] w-full lg:max-w-[40%] mx-auto p-6 space-y-12">
+      <ResumeContent />
+    </div>
+  );
 };
 
 export default ResumePage;
