e3670d282c3e94538fa936369af89b50e6904b52
정호연 <jea7730@gmail.com>
2024-09-17 02:26:58 +0900

    :hammer: 정호연 : 프로필 섹션 추가 및 스타일 수정

--- src/components/Resume/ResumeContent.tsx
+++ src/components/Resume/ResumeContent.tsx
@@ -3,33 +3,39 @@ import React from 'react';
 const ResumeContent: React.FC = () => {
   return (
     <>
+      {/* 프로필 섹션 */}
+      <section className="text-center lg:flex items-center">
+        <h1 className="text-3xl font-bold">정호연 | Nicknul</h1>
+        <img src="/imgs/Profile/emoji.jpeg" alt="Profile" className="w-32 rounded-full mx-auto mb-4" />
+      </section>
+
       {/* 자기 소개 섹션 */}
       <section>
-        <h2 className="text-2xl font-bold mb-4">자기 소개</h2>
+        <h2 className="text-2xl font-bold mb-4">ABOUT ME | Introduce</h2>
         <p>여기에 자기 소개 내용을 추가하세요.</p>
+      </section>
+
+      {/* 교육 섹션 */}
+      <section>
+        <h2 className="text-2xl font-bold mb-4">EDUCATION</h2>
+        <p>여기에 학력 정보를 추가하세요.</p>
       </section>
 
       {/* 스킬 섹션 */}
       <section>
-        <h2 className="text-2xl font-bold mb-4">스킬</h2>
+        <h2 className="text-2xl font-bold mb-4">TOOLS</h2>
         <p>여기에 사용할 수 있는 개발 언어 목록을 추가하세요.</p>
       </section>
 
       {/* 프로젝트 섹션 */}
       <section>
-        <h2 className="text-2xl font-bold mb-4">프로젝트</h2>
+        <h2 className="text-2xl font-bold mb-4">PROJECT</h2>
         <p>여기에 프로젝트 내용을 추가하세요.</p>
-      </section>
-
-      {/* 학력 섹션 */}
-      <section>
-        <h2 className="text-2xl font-bold mb-4">학력</h2>
-        <p>여기에 학력 정보를 추가하세요.</p>
       </section>
 
       {/* 근무 이력 섹션 */}
       <section>
-        <h2 className="text-2xl font-bold mb-4">근무 이력</h2>
+        <h2 className="text-2xl font-bold mb-4">CAREER</h2>
         <p>여기에 근무 이력을 추가하세요.</p>
       </section>
     </>
