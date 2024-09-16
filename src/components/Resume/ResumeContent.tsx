967038a09e1eafa284e855079ee5c2793e71e265
정호연 <jea7730@gmail.com>
2024-09-17 03:11:21 +0900

    :lipstick: 정호연 : 컨텍트 영역의 카테고리 스타일 수정

--- src/components/Resume/ResumeContent.tsx
+++ src/components/Resume/ResumeContent.tsx
@@ -17,18 +17,30 @@ const ResumeContent: React.FC = () => {
         {/* CONTACT 섹션 */}
         <div className="lg:ml-8">
           <h2 className="text-2xl font-bold mb-4">CONTACT</h2>
-          <p>Email: jea7730@gmail.com</p>
           <p>
-            GitHub:
-            <a href="https://github.com/Nicknul" className="text-blue-500">
+            <span className="font-semibold">Email </span>
+            <a href="mailto:jea7730@gmail.com" className="text-sm text-blue-500 ml-1">
+              jea7730@gmail.com
+            </a>
+          </p>
+          <p>
+            <span className="font-semibold">GitHub </span>
+            <a
+              href="https://github.com/Nicknul"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sm text-blue-500 ml-1"
+            >
               github.com/Nicknul
             </a>
           </p>
           <p>
-            Notion:
+            <span className="font-semibold">Notion </span>
             <a
               href="https://stupendous-lighter-a2b.notion.site/3285a71a0daf4475b8dabd1ca260604d?v=fc4dafec8dc8454098541b1f489dd710&pvs=4"
-              className="text-blue-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sm text-blue-500 ml-1"
             >
               notion.so/정호연의 기록
             </a>
