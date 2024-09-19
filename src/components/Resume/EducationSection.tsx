6b141d4cd5dca0f357490bf491c5fec2d67ed573
정호연 <jea7730@gmail.com>
2024-09-19 14:40:25 +0900

    :pencil2: 정호연 : 4기 > 2기로 수정

--- src/components/Resume/EducationSection.tsx
+++ src/components/Resume/EducationSection.tsx
@@ -24,7 +24,7 @@ const EducationSection: React.FC = () => (
           <p className="text-sm text-gray-500">2024.03 - 현재</p>
           <h3 className="text-lg font-semibold">그린컴퓨터아트학원</h3>
         </div>
-        <div className="md:text-base text-sm">KDT 지역인재 양성을 위한 IaaS기반 웹 서비스 개발자 과정 4기</div>
+        <div className="md:text-base text-sm">KDT 지역인재 양성을 위한 IaaS기반 웹 서비스 개발자 과정 2기</div>
       </div>
     </div>
   </section>
