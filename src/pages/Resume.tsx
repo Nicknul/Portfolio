5de32228707d2b897bf989fb7a3c3b4429d8f5b9
정호연 <jea7730@gmail.com>
2024-09-19 11:36:16 +0900

    :lipstick: 정호연 : 화면 영역 수정

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -3,7 +3,7 @@ import ResumeContent from '../components/Resume/ResumeContent';
 
 const ResumePage: React.FC = () => {
   return (
-    <div className="pt-16 min-h-[calc(100vh-4rem)] w-full xl:max-w-[40%] lg:max-w-[60%] mx-auto p-6 space-y-12">
+    <div className="pt-16 min-h-[calc(100vh-4rem)] w-full xl:max-w-[40%] md:max-w-[60%] mx-auto p-6 space-y-12">
       <ResumeContent />
     </div>
   );
