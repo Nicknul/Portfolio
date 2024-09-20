828919a43e1158a979e1e78f9982979b7cab7ace
정호연 <jea7730@gmail.com>
2024-09-21 03:30:12 +0900

    :adhesive_bandage: 정호연 : 이미지 데이터 수정

---
+++ src/data/ProjectImages.tsx
@@ -0,0 +1,8 @@
+export const projectImages = {
+  portfolio: ['/imgs/portfolio/image1.jpg', '/imgs/portfolio/image2.jpg', '/imgs/portfolio/image3.jpg'],
+  erp: ['/imgs/ERP/ERP_main.png', '/imgs/ERP/image2.jpg', '/imgs/ERP/image3.jpg'],
+  weather: ['/imgs/weather/image1.jpg', '/imgs/weather/image2.jpg', '/imgs/weather/image3.jpg'],
+  shop: ['/imgs/shop/image1.jpg', '/imgs/shop/image2.jpg', '/imgs/shop/image3.jpg'],
+  lunch: ['/imgs/lunch/image1.jpg', '/imgs/lunch/image2.jpg', '/imgs/lunch/image3.jpg'],
+  pokemon: ['/imgs/pokemon/image1.jpg', '/imgs/pokemon/image2.jpg', '/imgs/pokemon/image3.jpg'],
+};
