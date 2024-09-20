60e63a7c4d1130ab3309c2e4dfb19ddaf44d7577
정호연 <jea7730@gmail.com>
2024-09-21 05:18:49 +0900

    :fire: 정호연 : 코드 리펙토링 > 미사용 파일 삭제건

--- src/styles/siteStyles.ts
+++
@@ -1,9 +0,0 @@
-const siteStyles = {
-  siteContainer: 'px-4 py-2 rounded-lg transition-all duration-300 cursor-pointer w-[150px] md:w-[200px]',
-  hoverActive: 'bg-black text-white',
-  hoverInactive: 'bg-white text-black',
-  hoverText: 'mt-2 text-gray-500 text-sm md:text-base transition-all duration-300',
-  siteListContainer: 'flex space-x-4 md:flex-row md:space-y-0 md:space-x-4',
-};
-
-export default siteStyles;
