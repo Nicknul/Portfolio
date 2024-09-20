69f281423bcb6b1e58a04564529ebe9a288cc215
정호연 <jea7730@gmail.com>
2024-09-21 03:19:27 +0900

    :hammer: 정호연 : Link 사용하여 프로젝트 페이지 접속

--- src/components/Card/CardItem.tsx
+++ src/components/Card/CardItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface CardItemProps {
   image: string;
@@ -19,33 +20,36 @@ const CardItem: React.FC<CardItemProps> = ({ image, date, title, category, githu
     'bg-blue-200', // Django
     'bg-indigo-200', // PostgreSQL
   ];
+  const pageLink = `/portfolio/${title.toLowerCase().replace(/\s+/g, '-')}`;
 
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-      <img src={image} alt={title} className="w-full h-48 object-cover" />
-      <div className="p-6">
-        <p className="text-sm text-gray-500">{date}</p>
-        <h2 className="text-xl font-semibold text-gray-900">{title}</h2>
-        <p className={`mt-2 text-sm ${category === '팀프로젝트' ? 'text-blue-500' : 'text-green-500'}`}>{category}</p>
-        <div className="mt-4">
-          <a href={github} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline mr-4">
-            GitHub
-          </a>
-          <a href={notion} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
-            Notion
-          </a>
-        </div>
-        <div className="mt-4">
-          <div className="flex flex-wrap gap-2 mt-2">
-            {languages.map((language, index) => (
-              <span key={index} className={`px-2 py-1 text-xs ${colors[index % colors.length]} rounded-full`}>
-                {language}
-              </span>
-            ))}
+    <Link to={pageLink}>
+      <div className="bg-white shadow-lg rounded-lg overflow-hidden cursor-pointer">
+        <img src={image} alt={title} className="w-full h-48 object-cover" />
+        <div className="p-6">
+          <p className="text-sm text-gray-500">{date}</p>
+          <h2 className="text-xl font-semibold text-gray-900">{title}</h2>
+          <p className={`mt-2 text-sm ${category === '팀프로젝트' ? 'text-blue-500' : 'text-green-500'}`}>{category}</p>
+          <div className="mt-4">
+            <a href={github} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline mr-4">
+              GitHub
+            </a>
+            <a href={notion} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+              Notion
+            </a>
+          </div>
+          <div className="mt-4">
+            <div className="flex flex-wrap gap-2 mt-2">
+              {languages.map((language, index) => (
+                <span key={index} className={`px-2 py-1 text-xs ${colors[index % colors.length]} rounded-full`}>
+                  {language}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
