6a2a104a45ccda23f1108f8867d9f585a199c376
정호연 <jea7730@gmail.com>
2024-09-21 02:05:41 +0900

    :lipstick: 정호연 : 모바일 여백 추가

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -6,11 +6,9 @@ const PortfolioPage: React.FC = () => {
   const [selectedCategory, setSelectedCategory] = useState<'팀프로젝트' | '싱글' | '전체'>('전체');
 
   return (
-    <div className="pt-16 min-h-[calc(100vh-4rem)]">
-      <div className="w-full xl:max-w-[40%] lg:max-w-[70%] mx-auto">
-        <Toggle selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-        <CardList category={selectedCategory} />
-      </div>
+    <div className="pt-16 min-h-[calc(100vh-4rem)] w-full max-w-[90%] xl:max-w-[40%] lg:max-w-[70%] mx-auto">
+      <Toggle selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+      <CardList category={selectedCategory} />
     </div>
   );
 };
