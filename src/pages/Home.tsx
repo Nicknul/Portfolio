9bf0375256f7ab624f48b423adbcb275ce388c44
정호연 <jea7730@gmail.com>
2024-09-22 18:50:58 +0900

    :hammer: 정호연 : 이름, 소개 등 영역 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
 
 const HomePage: React.FC = () => {
-  return <div className="p-20">Home Page</div>;
+  return (
+    <div className="pt-16 min-h-[calc(100vh-4rem)] w-full max-w-[90%] xl:max-w-[40%] lg:max-w-[70%] mx-auto flex flex-col items-center justify-center text-center">
+      {/* 이름과 소개 */}
+      <h1 className="text-4xl font-bold mb-4">정호연</h1>
+      <p className="mb-6 max-w-lg">
+        안녕하세요! 신입 개발자 정호연입니다. <br /> <br />
+        저의 좌우명인 멈춤보다 실행처럼 끊임없이 접근하고 연구하며 <br />
+        계속해서 나아가도록 노력하고 있습니다.
+      </p>
+
+      {/* Resume와 Portfolio 버튼 */}
+      <div className="flex gap-4 mb-6">
+        <a
+          href="/resume"
+          className="px-6 py-2 border border-black rounded hover:bg-black hover:text-white transition duration-300"
+        >
+          Resume
+        </a>
+        <a
+          href="/portfolio"
+          className="px-6 py-2 border border-black rounded hover:bg-black hover:text-white transition duration-300"
+        >
+          Portfolio
+        </a>
+      </div>
+
+      {/* 소셜 미디어 링크 */}
+      <div className="flex gap-4 mb-6">
+        <a href="https://github.com/Nicknul" target="_blank" rel="noopener noreferrer">
+          <img src="/imgs/github.png" alt="GitHub" className="w-8 h-8" />
+        </a>
+        <a
+          href="https://stupendous-lighter-a2b.notion.site/3285a71a0daf4475b8dabd1ca260604d?v=fc4dafec8dc8454098541b1f489dd710&pvs=4"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/imgs/notion.png" alt="Notion" className="w-8 h-8" />
+        </a>
+      </div>
+
+      {/* Contact Me 버튼 */}
+      <a
+        href="jea7730@gmail.com"
+        className="px-6 py-2 border border-black rounded hover:bg-black hover:text-white transition duration-300 mb-6"
+      >
+        Contact Me
+      </a>
+    </div>
+  );
 };
 
 export default HomePage;
