e28a3357d5973993e390da844d876c130ac61aa7
정호연 <jea7730@gmail.com>
2024-09-22 20:55:57 +0900

    :package: 정호연 : 경로 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -9,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
-    publicPath: '/',
+    publicPath: './',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
@@ -31,37 +30,13 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       minify: {
-        // HTML 최적화 설정
         collapseWhitespace: true,
         removeComments: true,
-        removeRedundantAttributes: true,
         useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
       },
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
     }),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-  devServer: {
-    historyApiFallback: true,
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    compress: true,
-    port: 3001,
-    open: true,
-  },
 };
